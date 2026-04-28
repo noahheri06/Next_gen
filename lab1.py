071858a759538b3deb9ede8ee8236d8fa39586ed
tbello <T.J.Bello@student.tudelft.nl>
2026-04-28 12:06:39 +0200

    excerse 3 and beetje 2

--- lab1.py
+++ lab1.py
@@ -10,7 +10,6 @@ Z0 = 50
 def readout_s2p(your_file):
 
   ntwk = rf.Network(f'{your_file}.s2p')
-
   # Frequency (Hz)
   freq = ntwk.f
 
@@ -22,16 +21,11 @@ def readout_s2p(your_file):
   S21 = S[:, 1, 0]
   S12 = S[:, 0, 1]
   S22 = S[:, 1, 1]
-  return(freq, S11, S21, S12, S22)
+
+  return(freq, S, S11, S21, S12, S22)
 
 
-def getABCD(S11, S12, S21, S22):
-  A  = ((1+S11)*(1-S22) + S12*S21)/(2*S21)
-  B  = Z0*((1+S11)*(1+S22) - S12*S21)/(2*S21)
-  C  = ((1-S11)*(1-S22) - S12*S21)/((2*S21)*Z0)
-  D  = ((1-S11)*(1+S22) + S12*S21)/(2*S21)
-  matrixABCD = np.array([[A, B], [C, D]])
-  return (matrixABCD)
+
 
 def plot_smith_charts(measured_file, given_file, m=0, n=0):
    # Create Network objects (needed for smith plotting)
@@ -53,8 +47,8 @@ def plot_smith_charts(measured_file, given_file, m=0, n=0):
 #for recalibrate measurement l=1.84cm
 
 def method1():
-  frequency, S11, S21, _, _ = readout_s2p("recalibrate")
-  frequency = frequency[:-3*len(frequency)//7]
+  frequency, _, S11, S21, _, _ = readout_s2p("recalibrate")
+  frequency = frequency[:-3*len(frequency)//10]
   S11 = S11[:len(frequency)]
   S21 = S21[:len(frequency)]
   phase = np.angle(S11, deg=False)
@@ -69,8 +63,15 @@ def method1():
   plt.ylabel("Magnitude (dB)")
   plt.grid()
 
-  time_delay = -np.gradient(phase, frequency*(2*np.pi))
   plt.subplot(2, 1, 2)
+  plt.plot(frequency, phase)
+  plt.title("Phase of S11")
+  plt.xlabel("Frequency (Hz)")
+  plt.ylabel("phase (rad)")
+  plt.grid()
+
+  time_delay = -np.gradient(phase, frequency*(2*np.pi))
+  plt.subplot(2, 1, 3)
   plt.plot(frequency, time_delay)
   plt.ylim(0, 1e-10)
   plt.title("Time Delay")
@@ -90,26 +91,26 @@ def interpolate_complex(x_old, y_old, x_new):
     imag_interp = np.interp(x_new, x_old, np.imag(y_old))
     return real_interp + 1j * imag_interp
 
-def interpolate_sparameters(x_old, y_old, x_new):
-    # y_old: shape (2, 2, N)
+def interpolate_sparameters(x_old, y_old, x_new): #freq tot, abcdtot, freq_probe
 
-    y_new = np.zeros((2, 2, len(x_new)), dtype=complex)
+
+    y_new = np.zeros((len(x_new), 2, 2), dtype=complex)
 
     for i in range(2):
         for j in range(2):
-            real_interp = np.interp(x_new, x_old, np.real(y_old[i, j, :]))
-            imag_interp = np.interp(x_new, x_old, np.imag(y_old[i, j, :]))
-            y_new[i, j, :] = real_interp + 1j * imag_interp
+            real_interp = np.interp(x_new, x_old, np.real(y_old[:, i, j]))
+            imag_interp = np.interp(x_new, x_old, np.imag(y_old[:, i, j]))
+            y_new[:, i, j] = real_interp + 1j * imag_interp
 
     return y_new
 
 
 def method2():
-    frequency_measured, S11_measured, S21_measured, _, _ = readout_s2p("recalibrate")
+    frequency_measured, _, S11_measured, S21_measured, _, _ = readout_s2p("recalibrate")
     S21_measured = S21_measured[:len(frequency_measured)]
     S11_measured = S11_measured[:len(frequency_measured)]
 
-    frequency_given, S11_given, S21_given, _, _ = readout_s2p("thru_calibrated")
+    frequency_given, _, S11_given, S21_given, _, _ = readout_s2p("thru_calibrated")
     S21_given = S21_given[:len(frequency_given)]
     S11_given = S11_given[:len(frequency_given)]
 
@@ -131,35 +132,103 @@ def method2():
 
 
 def excersice2(measuered_transmission, given_probe):
-  frequency_TOT, S11_TOT, S12_TOT, S21_TOT, S22_TOT =readout_s2p(measuered_transmission)
-  frequency_probe, S11_probe, S12_probe, S21_probe, S22_probe =readout_s2p(given_probe)
+  frequency_TOT, smat_TOT, S11_TOT, S21_TOT, S12_TOT, S22_TOT =readout_s2p(measuered_transmission)
+  frequency_probe, smat_probe, S11_probe, S21_probe, S12_probe, S22_probe =readout_s2p(given_probe)
 
-  ABCD_probe = getABCD(S11_probe, S12_probe, S21_probe, S22_probe)
-  ABCD_TOT = getABCD(S11_TOT, S12_TOT, S21_TOT, S22_TOT)
+  ABCD_probe = rf.network.s2a(smat_probe, Z0)
+  ABCD_TOT = rf.network.s2a(smat_TOT, Z0)
   ABCD_TOT = interpolate_sparameters(frequency_TOT, ABCD_TOT, frequency_probe)
 
-  ABCD_probe_inv = np.linalg.inv(np.moveaxis(ABCD_probe, 2, 0)).transpose(1, 2, 0)
-  result = np.matmul(ABCD_probe_inv.transpose(2,0,1), ABCD_TOT.transpose(2,0,1))
-  result = np.matmul(result, ABCD_probe_inv.transpose(2,0,1))
-  ABCD_LINE = result.transpose(1,2,0)
 
-  S_LINE = rf.a2s(np.moveaxis(ABCD_LINE, 2, 0), z0=Z0)
+  ABCD_LINE = []
+
+  for i in range(len(frequency_probe)):
+    ABCD_probe_i = ABCD_probe[i]
+    ABCD_TOT_i = ABCD_TOT[i]
+    ABCD_probe_i_inv = np.linalg.inv(ABCD_probe_i)
+
+
+    result = np.matmul(ABCD_probe_i_inv, ABCD_TOT_i)
+    result = np.matmul(result, ABCD_probe_i_inv)
+    ABCD_LINE.append(result)
+
+  #print(ABCD_LINE)
+  ABCD_LINE = np.array(ABCD_LINE)
+
+  S_LINE = rf.network.a2s(ABCD_LINE, Z0)
   plt.subplot(2, 1, 1)
-  plt.plot(frequency_TOT, 20*np.log10(np.abs(S11_TOT)), label="TOT")
-  plt.plot(frequency_probe, 20*np.log10(np.abs(S_LINE[:, 0, 0])), label="LINE")
+  plt.title("Frequency vs Magnitude")
+  plt.xlabel("Frequency (GHz)")
+  plt.ylabel("Magnitude (dB)")
+  plt.ylim(-40,0)
+  plt.plot(frequency_TOT/1e9, 20*np.log10(np.abs(S11_TOT)), label="TOT")
+  plt.plot(frequency_probe/1e9, 20*np.log10(np.abs(S_LINE[:, 0, 0])), label="LINE")
   plt.grid()
-  plt.legend()
+  plt.legend(loc="upper right")
 
   plt.subplot(2, 1, 2)
-  plt.plot(frequency_TOT, np.angle(S11_TOT), label="TOT")
-  plt.plot(frequency_probe, np.angle(S_LINE[:, 0, 0]), label="LINE")
+  plt.title("Frequency vs Phase")
+  plt.xlabel("Frequency (GHz)")
+  plt.ylabel("Phase (Rad)")
+  plt.plot(frequency_TOT/1e9, np.angle(S11_TOT), label="TOT")
+  plt.plot(frequency_probe/1e9, np.angle(S_LINE[:, 0, 0]), label="LINE")
   plt.grid()
-  plt.legend()
+  plt.legend(loc="upper right")
+
+
+  plt.tight_layout()
+  #plt.savefig("magnitude and freq exc2.png", dpi=600)
   plt.show()
 
+def exercise3(impedance):
+  freq, matrixs, S11, _, _, _ = readout_s2p(impedance)
+  p_reflected = np.abs(S11)**2
+
+
+  a = 138 # 26 = 1.4 GHz. 138 = 7 GHz
+  freq = freq/1e9
+
+  
+  print(f"power reflected at {freq[a]} Ghz  = { 100*p_reflected[a]} %)")
+
+  p_reflected = 10*np.log10(p_reflected)
+
+  print(f" which equals {p_reflected[a]} dB)")
+
+  plt.plot()
+  plt.title("Frequency vs Magnitude")
+  plt.xlabel("Frequency (GHz)")
+  plt.ylabel("Magnitude (dB)")
+  plt.ylim(-40,0)
+  plt.plot(freq, p_reflected)
+  plt.grid()
+  plt.show()
+
+  matrixz = rf.network.s2z(matrixs)
+  Z11 = matrixz[:,0,0]
+
+  # plt.plot(freq, np.real(Z11) > 50)
+  # plt.show()
+
+  Rl = np.real(Z11[a])
+  Xl = np.imag(Z11[a])
+  if(Rl > Z0):
+    print("Step down needed")
+    B = Xl + ((Rl/Z0)**(1/2) * (Rl**2 + Xl**2 - Rl*Z0)**(1/2))/(Rl**2 + Xl**2) #+ or - choice
+    X = 1/B + Xl*Z0/Rl - Z0/(B*Rl)
+    print(f"B = {B}, X = {X}")
+
+  else:
+    print("Step up needed")
+    X = (Rl(Z0 - Rl))**(1/2) - Xl
+    B = (((Z0-Rl)/Rl)**(1/2))/(Z0)
+    print(f"B = {B}, X = {X}")
+    #+ or - choice twice, both should have same sign
+  return
 
 if __name__ == "__main__":
   #method1()
-  method2()
+  #method2()
   #excersice2("exerciswe2beter", "XMW_probe")
+  exercise3("week1excercise3")
   pass
