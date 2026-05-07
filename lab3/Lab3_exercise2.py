fb2467417915c895bc3f67988e814ba0244252d3
rubC0d3r <rubenjochemsen@gmail.com>
2026-05-07 17:09:35 +0200

    iets beter idk

--- lab3/Lab3_exercise2.py
+++ lab3/Lab3_exercise2.py
@@ -13,6 +13,12 @@ dy = lambd/2
 
 
 
+
+# =============================================================================
+# A
+# =============================================================================
+
+
 # =============================================================================
 # 3D PLOT
 # =============================================================================
@@ -148,7 +154,7 @@ middlebeam = groups[floor(len(groups)/2)]
 beamwidthzero = theta[middlebeam[-1]]-theta[middlebeam[0]]
 beamwidthzero = np.rad2deg(beamwidthzero)
 
-
+print("8 in a line")
 print(f"The beamwidth in the phi=90 plane is {beamwidth90} deg")
 print(f"The beamwidth in the phi=0 plane is {beamwidthzero} deg")
 
@@ -156,9 +162,175 @@ D = 4*np.pi*(180/np.pi)**2 / (beamwidthzero*beamwidth90)
 
 print(f"The directivity of the antenna is {round(D,3)}, which is {round(20*np.log10(D),4)} dB")
 
+print(10*"-")
+
+#%%
+# =============================================================================
+# B
+# =============================================================================
+
+L = 0.02
+W = 0.03
+
+er = 3.44
+tan_delta = 0.01
+h = 0.5e-3
+
+
+theta = np.linspace(0, np.pi, 1000)
+phi = np.linspace(-np.pi, np.pi, 1000)
+
+THETA, PHI = np.meshgrid(theta, phi)
+    
+
+
+vx = (L/lambd) * np.sin(THETA) * np.cos(PHI)
+vy = (W/lambd) * np.sin(THETA) * np.sin(PHI)
+
+f = np.cos(vx*np.pi) * np.sinc(vy)
+
+pattern = (np.cos(THETA)**2 * np.sin(PHI)**2 + np.cos(PHI)**2) * np.abs(f)**2
+
+pattern_norm = pattern/np.max(pattern)
+
+
+# plotting
+X = pattern_norm * np.sin(THETA) * np.cos(PHI)
+Y = pattern_norm * np.sin(THETA) * np.sin(PHI)
+Z = pattern_norm * np.cos(THETA)
+
+colors = cm.jet(pattern_norm)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.plot_surface(X,Y,Z, facecolors = colors)
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.set_title("Array Factor")
+
+plt.grid()
+plt.show()
+#%%
+
+# =============================================================================
+# D
+# =============================================================================
+
+
+# 3D PLOT
+
+dx=dy
+
+phi = np.linspace(0,np.pi,300)
+theta = np.linspace(-np.pi,np.pi,300)
+
+THETA,PHI = np.meshgrid(theta,phi)
+
+
+AF = np.zeros(np.shape(THETA))
+
+for n in range(8):
+    for m in range(8):
+    
+        kvect = k0*np.array([np.sin(THETA)*np.cos(PHI), np.sin(THETA)*np.sin(PHI), np.cos(THETA)])
+        
+        rvect = np.array([dx*m,dy*n,0])
+        
+        kdotr = kvect[0]*rvect[0]+kvect[1]*rvect[1]+kvect[2]*rvect[2]
+        
+        AF = AF + np.e**(1j*kdotr)
+
+
+AF_mag = np.abs(AF)
+
+AF_norm = AF_mag/np.max(AF_mag)
+    
+
+R = AF_norm
+
+X = R * np.sin(THETA)*np.cos(PHI)
+Y = R * np.sin(THETA)*np.sin(PHI)
+Z = R * np.cos(THETA)
+
+colors = cm.jet(AF_norm)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.plot_surface(X,Y,Z, facecolors = colors)
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.set_title("Array Factor")
+
+plt.grid()
+plt.show()
 
 
 
+# CALCULATING DIRECTIVITY
 
 
+
+phi = np.array([0,np.pi/2])
+theta = np.linspace(-np.pi,np.pi,10000)
+
+THETA,PHI = np.meshgrid(theta,phi)
+AF = np.zeros(np.shape(THETA))
+
+for n in range(8):
+    for m in range(8):
+    
+        kvect = k0*np.array([np.sin(THETA)*np.cos(PHI), np.sin(THETA)*np.sin(PHI), np.cos(THETA)])
+        
+        rvect = np.array([dx*m,dy*n,0])
+        
+        kdotr = kvect[0]*rvect[0]+kvect[1]*rvect[1]+kvect[2]*rvect[2]
+        
+        AF = AF + np.e**(1j*kdotr)
+
+AF_mag = np.abs(AF)
+
+AF_norm = AF_mag/np.max(AF_mag)
+    
+AF_zerophi = AF_norm[0]
+AF_90phi = AF_norm[1]
+
+power_zerophi = AF_zerophi**2
+power_90phi = AF_90phi**2
+
+ind_zerophi = np.where(power_zerophi>0.5)[0]
+ind_90phi = np.where(power_90phi>0.5)[0]
+
+# for phi = 90
+splits = np.where(np.diff(ind_90phi) !=1)[0] + 1
+groups = np.split(ind_90phi,splits)
+
+middlebeam = groups[floor(len(groups)/2)]
+
+beamwidth90 = theta[middlebeam[-1]]-theta[middlebeam[0]]
+beamwidth90 = np.rad2deg(beamwidth90)
+
+
+# for phi = 0
+splits = np.where(np.diff(ind_zerophi) !=1)[0] + 1
+groups = np.split(ind_zerophi,splits)
+
+middlebeam = groups[floor(len(groups)/2)]
+
+beamwidthzero = theta[middlebeam[-1]]-theta[middlebeam[0]]
+beamwidthzero = np.rad2deg(beamwidthzero)
+
+print("8x8 matrix")
+print(f"The beamwidth in the phi=90 plane is {beamwidth90} deg")
+print(f"The beamwidth in the phi=0 plane is {beamwidthzero} deg")
+
+D = 4*np.pi*(180/np.pi)**2 / (beamwidthzero*beamwidth90)
+
+print(f"The directivity of the antenna is {round(D,3)}, which is {round(20*np.log10(D),4)} dB")
 
