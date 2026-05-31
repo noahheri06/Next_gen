3f5e8dcd1b3afc80b513fa21ec5e774fb0a463b8
rubC0d3r <rubenjochemsen@gmail.com>
2026-05-31 20:46:51 +0200

    3b fix

--- lab6/exercise 3.py
+++ lab6/exercise 3.py
@@ -32,35 +32,59 @@ f_c = 12e6 # arbitrary
 F_s = 50e6 # sampling freq
 
 
-signal = np.zeros(int(CPI * F_s))
+signal = np.zeros(int(CPI * F_s), dtype=complex)
 
 
 samples_in_one_pulse = int(F_s * tau_pulse)
-t = np.linspace(0, tau_pulse, num=samples_in_one_pulse)
-one_pulse = np.sin(2*np.pi*f_c*t)
+t = np.arange(samples_in_one_pulse) / F_s
+one_pulse = np.exp(2j*np.pi*f_c*t)
 
 
-PRI_samples = F_s * PRI
+PRI_samples = int(round(F_s * PRI))
 
 for i in range(40):
-    signal[int(i*PRI_samples):int(i*PRI_samples + samples_in_one_pulse)] = one_pulse
+    signal[i*PRI_samples:i*PRI_samples + samples_in_one_pulse] = one_pulse
     
     
     
 # take fft
-Signal = np.abs(fftshift(fft(signal, n = int(2**(np.ceil(np.log2(len(signal)))+2)))))
+Signal = np.abs(fftshift(fft(signal, n = int(2**(np.ceil(np.log2(len(signal)))+3)))))
 
 #normalize
 Signal = Signal/np.max(Signal)
 
 
-freqax = np.linspace(-F_s/2,F_s/2,num=len(Signal))/1e6
+freqax = np.linspace(-F_s/2,F_s/2,num=len(Signal))
 
-fig, ax = plt.subplots(1,1)
+fig, (ax3,ax2,ax) = plt.subplots(3,1)
 ax.plot(freqax,Signal)
-ax.plot(freqax, np.abs(np.sinc(freqax-f_c/1e6))) # plot sinc function for reference
-# ax.set_xlim(11e6,13e6)
-ax.set_xlabel("Frequency (MHz)")
+ax2.plot(freqax,Signal)
+ax3.plot(freqax,Signal)
+
+
+ax.set_xlim(11.88,12.12)
+ax.set_xlim(11.9999,12.0001)
+
+ax.set_xlim(11_999_900,12_000_100)
+ax2.set_xlim(11_995_000,12_005_000)
+ax3.set_xlim(11_880_000,12_120_000)
+
+ax.axhline(0.05,xmin=0.5+0.01, xmax=0.625+0.01, color = "red",label="Rayleigh width = 25Hz")
+ax.axvline(12_000_002)
+ax2.axhline(0.4,xmin=0.5,xmax=0.6,color="red", label="Spacing of line segments = 1 kHz")
+
+
+ax.set_xlabel("Frequency (Hz)")
+ax2.set_xlabel("Frequency (Hz)")
+ax3.set_xlabel("Frequency (Hz)")
+
+
+ax.set_ylabel("Normalized amplitude")
+ax2.set_ylabel("Normalized amplitude")
+ax3.set_ylabel("Normalized amplitude")
+
+ax.legend(loc=1)
+ax2.legend(loc=1)
 plt.show()
 
 
